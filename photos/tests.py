4b792c17f52a5e9fbaec9481fdcd2f6b6e14228c
naimar1 <naiglyme1@gmail.com>
2019-03-19 13:46:08 +0200

    Add the test for category model

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
-
+from .models import Image,Category,Location
 # Create your tests here.
+
+class CategoryTestCase(TestCase):
+
+    def setUp(self):
+        self.sport = Category(name = 'Sport')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.sport, Category))
+
+    def tearDown(self):
+        Category.objects.all().delete()
+
+    def test_save_category(self):
+        self.sport.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def test_delete_category(self):
+        self.sport.delete_category('Sport')
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) == 0)
+
