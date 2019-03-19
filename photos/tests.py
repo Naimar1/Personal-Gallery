c32de4d502ea486c4ce65a43aa853cfbbef2c147
naimar1 <naiglyme1@gmail.com>
2019-03-19 13:47:47 +0200

    Add the test for image model

--- photos/tests.py
+++ photos/tests.py
@@ -43,4 +43,26 @@ class LocationTestCase(TestCase):
         locations = Location.objects.all()
         self.assertTrue(len(locations) == 0)
 
+class ImageTestCase(TestCase):
+    def setUp(self):
+        # Creating a new category and saving it
+        self.new_category = Category(name = 'Sport')
+        self.new_category.save_category()
+
+        # Creat a new location and saving it
+        self.new_location = Location(name = 'Spain')
+        self.new_location.save_location()
+
+        self.new_image = Image(name = 'Test Image',description = 'Test Description',category = self.new_category,location = self.new_location)
+        self.new_image.save_image()
+
+    def tearDown(self):
+        Category.objects.all().delete()
+        Location.objects.all().delete()
+        Image.objects.all().delete()
+
+    def test_save_image(self):
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
 
